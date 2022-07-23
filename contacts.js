de43b557d81ac8019877b27d9d8381939fa3554e
Megan Chantosa <megan@Megans-MacBook-Air.local>
2022-07-23 11:13:06 -0700

    added flash messaging

--- contacts.js
+++ contacts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const session = require('express-session');
 const store = require('connect-loki');
+const flash = require('express-flash');
 const { body, validationResult } = require('express-validator');
 
 const app = express();
@@ -66,11 +67,19 @@ app.use(session({
   store: new LokiStore({}),
 }));
 
+app.use(flash());
+
 app.use((req, res, next) => {
   if (!('contactData' in req.session)) {
     req.session.contactData = clone(contactData);
   }
 
+  next();
+});
+
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
   next();
 });
 
@@ -116,8 +125,9 @@ app.post(
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      errors.array().forEach((error) => req.flash('error', error.msg));
       res.render('new-contact', {
-        errorMessages: errors.array().map((error) => error.msg),
+        flash: req.flash(),
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         phoneNumber: req.body.phoneNumber,
@@ -134,6 +144,7 @@ app.post(
       phoneNumber: req.body.phoneNumber,
     });
 
+    req.flash('success', 'New contact added to list!');
     res.redirect('/contacts');
   },
 );
--- readme.md
+++ readme.md
@@ -5,4 +5,7 @@ This project is a more complex intro to Express than [hello-world-express](https
 
 **Examples:**
 * **Use a form:** created `new-contact` view using form data with corresponding `get` and `post` handlers.
-* **Validate user input data:**
+* **Validate user input data:** Added validation middleware to `/contact/new` handler
+* **Persisted user data over validation failures**
+* **Set up cookies and session persistance:** used `express-session` and `connect-loki` libraries to faciligtate this. The session secret is included in the source control (since this is a tiny application intended to run locally). IRL, the session secret should not be under source control.
+* **Instroduces flash messaging:** flash messaging is introduced to persisted messages across views
